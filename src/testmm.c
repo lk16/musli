18a00c891388942e5f23ba2f775086359a1dc04c
Luuk Verweij <luuk_verweij@msn.com>
2015-02-24 22:58:38 +0100

    implemented pvs and mtdf succesfully in testmm.c

--- src/testmm.c
+++ src/testmm.c
@@ -47,7 +47,7 @@ int test_minimax(const struct board* b,int depth_remaining,int is_max){
   return best;
 }
 
-int test_negamax(const struct board* b,int depth_remaining,int is_max){
+int test_negamax(const struct board* b,int depth_remaining){
   if(depth_remaining==0){
     return dummy_heur(b);
   }
@@ -55,7 +55,7 @@ int test_negamax(const struct board* b,int depth_remaining,int is_max){
   struct board children[32];
   struct board *it,*child_end = board_get_children(b,children);
   for(it=children;it!=child_end;it++){
-    int cur = -test_negamax(it,depth_remaining-1,!is_max);
+    int cur = -test_negamax(it,depth_remaining-1);
     if(cur > best){
       best = cur;
     }
@@ -81,6 +81,57 @@ int test_alphabeta(const struct board* b,int depth_remaining,int alpha,int beta)
   return alpha;
 }
 
+int test_pvs(const struct board* b,int depth_remaining,int alpha,int beta){
+  if(depth_remaining==0){
+    return dummy_heur(b);
+  }
+  struct board children[32];
+  struct board *it,*child_end = board_get_children(b,children);
+  for(it=children;it!=child_end;it++){
+    int score;
+    if(it==children){
+      score = -test_pvs(it,depth_remaining-1,-beta,-alpha);
+    }
+    else{ 
+      score = -test_pvs(it,depth_remaining-1,-alpha-1,-alpha);
+      if((alpha < score) && (score < beta)  ){
+        score = -test_pvs(it,depth_remaining-1,-beta,-score);
+      }
+    }
+    if(score >= beta){
+      alpha = beta;
+      break;
+    }
+    if(score > alpha){
+      alpha = score;
+    }
+  
+  }
+  return alpha;
+}
+
+int test_mtdf(const struct board* b,int depth_remaining,int f){
+  int g = f;
+  int high = 64000;
+  int low = -64000;
+  int beta;
+  while(low < high){
+    if(g == low){
+      beta = g + 1;
+    }
+    else{
+      beta = g;
+    }
+    g = test_pvs(b,depth_remaining,beta-1,beta);
+    if(g < beta){
+      high = g;
+    }
+    else{
+      low = g;
+    }
+  }
+  return g;  
+}
 
 int arg_run_minimax_tests(struct game_config* gc,const struct parse_state* ps){
   (void)ps;
@@ -88,13 +139,31 @@ int arg_run_minimax_tests(struct game_config* gc,const struct parse_state* ps){
   struct board b;
   board_init(&b);
   board_do_random_moves(&b,10);
-
+  
+  enum{
+    MINIMAX_MAX_DEPTH = 6,
+    NEGAMAX_MAX_DEPTH = 6,
+    ALPHA_BETA_MAX_DEPTH = 8,
+    PVS_MAX_DEPTH = 9,
+    MTDF_MAX_DEPTH = 11
+  };
+  
+  
   int depth;
-  for(depth=1;depth<7;depth++){
+  for(depth=1;depth<60;depth++){
+    if(depth > MTDF_MAX_DEPTH){ continue; }
+    printf("%s","\n");
+    printf("mtdf at depth %d:\t%d\n",depth,test_mtdf(&b,depth,0));
+    if(depth > PVS_MAX_DEPTH){ continue; }
+    printf("pvs at depth %d:\t\t%d\n",depth,test_pvs(&b,depth,-64000,64000));
+    if(depth > ALPHA_BETA_MAX_DEPTH){ continue; }
+    printf("alphabeta at depth %d:\t%d\n",depth,test_alphabeta(&b,depth,-64000,64000));
+    if(depth > NEGAMAX_MAX_DEPTH){ continue; }
+    printf("negamax at depth %d:\t%d\n",depth,test_negamax(&b,depth));
+    if(depth > MINIMAX_MAX_DEPTH){ continue; }
     printf("minimax at depth %d:\t%d\n",depth,test_minimax(&b,depth,1));
-    printf("negamax at depth %d:\t%d\n",depth,test_negamax(&b,depth,1));
-    printf("alphabeta at depth %d:\t%d\n\n",depth,test_alphabeta(&b,depth,-64000,64000));
   }
+  printf("%s","\n");
   exit(0); // ugly
   return 1;
 }
--- src/testmm.h
+++ src/testmm.h
@@ -11,9 +11,10 @@
 int dummy_heur(const struct board* b);
 
 int test_minimax(const struct board* b,int depth_remaining,int is_max);
-int test_negamax(const struct board* b,int depth_remaining,int is_max);
+int test_negamax(const struct board* b,int depth_remaining);
 int test_alphabeta(const struct board* b,int depth_remaining,int alpha,int beta);
-
+int test_pvs(const struct board* b,int depth_remaining,int alpha,int beta);
+int test_mtdf(const struct board* b,int depth_remaining,int f);
 
 
 int arg_run_minimax_tests(struct game_config* gc,const struct parse_state* ps);
